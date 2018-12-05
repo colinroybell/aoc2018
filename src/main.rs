49f968b485402cabec483c8e615c139111c696df
Colin Bell <colin@ps116.org.uk>
2018-12-05 08:10:12 +0000

    Part 1 of day 5

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ mod day01;
 mod day02;
 mod day03;
 mod day04;
+mod day05;
 
 extern crate regex;
 
@@ -16,6 +17,7 @@ fn main() {
             2 => day02::run(),
             3 => day03::run(),
             4 => day04::run(),
+            5 => day05::run(),
             _ => println!("Can't run day {}",day),
         } 
     } else {
