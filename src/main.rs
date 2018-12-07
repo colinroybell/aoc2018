78e3cdffee8c0339a448cac9df070b312f119c70
Colin Bell <colin@ps116.org.uk>
2018-12-07 11:11:14 +0000

    Days 6 and 7 src/day05.rs

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ mod day02;
 mod day03;
 mod day04;
 mod day05;
+mod day06;
+mod day07;
 
 extern crate regex;
 
@@ -18,6 +20,8 @@ fn main() {
             3 => day03::run(),
             4 => day04::run(),
             5 => day05::run(),
+            6 => day06::run(),
+            7 => day07::run(),
             _ => println!("Can't run day {}",day),
         } 
     } else {
